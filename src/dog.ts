09aa2ed2c54dd71cecf3b3c10a2a6b9839f6c6bf
Johan Herscheid <johanherscheid@gmail.com>
2018-06-18 19:46:53 +0200

    Added more console for startup

--- src/dog.ts
+++ src/dog.ts
@@ -18,5 +18,7 @@ class Dog extends Animal {
 // -------------------------
 var dog = new Dog('Woef');
 var dog2 = new Dog(23);
-console.log(dog.numberOfLegs); // 4
-console.log(dog.barkStyle);
+console.log(`dog: ${dog.numberOfLegs}`);
+console.log(`dog: ${dog.barkStyle}`);
+console.log(`dog2: ${dog2.numberOfLegs}`);
+console.log(`dog2: ${dog2.barkStyle}`);
